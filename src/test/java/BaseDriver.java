7875714c96f04bb7355176ceaac2ca1c43a543ef
nurev <nurevsankurak@gmail.com>
2024-02-22 13:17:18 +0300

    Base Driver

--- src/test/java/BaseDriver.java
+++ src/test/java/BaseDriver.java
@@ -1,2 +1,30 @@
+import java.time.Duration;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+
 public class BaseDriver {
+    private static WebDriver driver;
+
+    public static WebDriver getDriver() {
+        if (driver == null) {
+
+            Logger logger = Logger.getLogger("");
+            logger.setLevel(Level.SEVERE);
+            driver = new ChromeDriver();
+            driver.manage().window().maximize();
+            driver.manage().timeouts().pageLoadTimeout(Duration.ofSeconds(30));
+            driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(30));
+        }
+
+
+        return driver;
+    }
+
+    public static void quitDriver() {
+        if (driver != null) {
+            driver.quit();
+            driver = null;
+        }
+    }
 }
+
