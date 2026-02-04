7823b7931f45ed766698f1ea71031bebd1e2e37d
Nurevsan Kurak <92542957+nurevsankurak@users.noreply.github.com>
2026-02-04 23:02:05 +0300

    Update TestCase_01.java

--- src/test/java/Pages/TestCase_01.java
+++ src/test/java/Pages/TestCase_01.java
@@ -2,7 +2,7 @@ package Pages;
 
 import Utilities.BaseDriver;
 
-public class TestCase_01 extends BaseDriver {
+public class TestCase_1 extends BaseDriver {
     driver.get("https://www.amazon.com.tr/");
 }
 
